32a6f87dea3f9dd7b0375e493ccd37f80e03caa5
Lundhe <glundh1996@gmail.com>
2026-06-11 22:14:02 +0200

    uppgift 1, skall påbörja uppgift.

--- Test_search.py
+++ Test_search.py
@@ -1,63 +1,39 @@
 # # # 2.
-# def sum_list(list):
-#     total = 0
-#     for i in list:
-#         total+= i
-#     return total
-#
-#
-# def test_empty_list():
-#     expected = 0
-#     actual = sum_list([])
-#     assert actual == expected
-#
-#
-# def test_number_list():
-#     assert sum_list([5]) == 5
-#     assert sum_list([5, 5]) == 10
-#     assert sum_list([1, 2, 2, 1, 2, 2]) == 10
-#     # testet failade alltid när return = None
-#     # Vi måste därmed definera en loop funktion som går igenom list steg för steg och plussar på värdet i varje i
+from main import *
 
-    #. 3A & 3B
-    # Returnerar ett heltal med antalet vokaler som finns i ordet (aeiouyåäö)
-    # just nu finns inget i funktionen som faktiskt identifierar vokaler.
-# def count_vowels(word):
-#     total = 0
-#     for vowel in word:
-#         if vowel in "AEIOUYÅÄÖaeiouyåäö":
-#             total += 1
-#     return total
-#
-# def detect_vowels(word):
-#     expected = 0
-#     for vowel in "AEIOUYÅÄÖaeiouyåäö":
-#         if vowel == "AEIOUYÅÄÖaeiouyåäö":
-#             expected += 1
-#     actual = count_vowels(word)
-#     assert actual == expected # Denna verkar inte funka alls, tester kommer ut som pass men ah jag vet inte.
-#
-# def test_no_vowels():
-#     assert count_vowels("qwrt") == 0
-#     assert count_vowels("Tt") == 0
-#     assert count_vowels("123 123") == 0
-#     assert count_vowels("") == 0
 
-def max_number(lista):
-    max = 0
-    if len(lista) == 0:
-        return None
-    for i in lista:
-        if i > max:
-            max = i
-    return max
+def test_empty_list():
+    expected = 0
+    actual = sum_list([])
+    assert actual == expected
+# Vår expectetion i är summan på listan skall vara 0, actual är vår current lista "sum_list" sen assertar vi actual list som expected värde.
 
-def find_max_number(lista):
-    max = 0
-    for i in max_number([]):
-        if i > max:
-            max = i
+def test_number_list():
+    assert sum_list([5]) == 5
+    assert sum_list([5, 5]) == 10
+    assert sum_list([1, 2, 2, 1, 2, 2]) == 10
+   # Test cases som just nu testar att elementen plussas ihop och blir adderade.
+
+
+
+
+
+def test_no_vowels():
+    assert count_vowels("qwrt") == 0
+    assert count_vowels("Tt") == 0
+    assert count_vowels("123 123") == 0
+    assert count_vowels("") == 0
+# Testen just nu är fokuserar på att ej innehålla några vokaler
+
 
 def test_max_number():
-    assert max_number([])
-    assert max_number([1,2,3])
+    assert max_number([]) == None
+    assert max_number([1,2,9,3]) == 9
+    # Test som kollar att vi får ut none om listan är tom och att vi får det största värde ur en osorterad lista.
+
+def test_find_2nd_max():
+    assert find_2nd_max([2,9,8,9,3,10,6]) == 9
+    assert find_2nd_max([]) == None
+    assert find_2nd_max([1]) == None
+    #i detta test testar värdet som är näst störst i en lista kommer ut som = värdet
+    # Vi testar även om listan endast innehåller ett element eller inget element då returnar None.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #1.
 #1a. x<100: har bara 2 över och under
+from os import remove
+
 
 #1b. y == 42: har 2, antingen är y exakt 42 eller inte.
 
@@ -20,18 +22,47 @@
 # Rätt
 
 #2 # Returnerar summan av alla tal i listan
-def sum_list(list):
-    return None
 
-def test_empty_list():
-    expected = 0
-    actual   = sum_list([])
-    assert actual == expected
+# # # 2.
+def sum_list(list): # Funktion som plussar varje element i listan, för varje i (element) blir det totalen som sedan plussas med nästa i (element
+    total = 0
+    for i in list:
+        total+= i
+    return total
 
-def test_number_list():
-    assert sum_list([5]) == 5
-    assert sum_list([5,5]) == 10
-    assert sum_list([1,2,2,1,2,2]) == 10
+   #-----------------------------------------------------------------------------------------------------------#
+
+# 3A & 3B
+def count_vowels(word):
+    total = 0
+    for vowel in word:
+        if vowel in "AEIOUYÅÄÖaeiouyåäö":
+            total += 1
+    return total
+#Total är noll, för varje vowel i listan list, om vovel är någon av små eller stora vokaler plussas totalen med 1.
+
+    #-----------------------------------------------------------------------------------------------------------#
+
+def max_number(lista):
+    if len(lista) == 0:
+        return None
+    biggest = max(lista)
+    return biggest
+# för kollar funktionen om listan är tom ==0 då returnar None, annars blir variablen biggest största värdet i listan vilket representerar max(lista) gör.
+   #-----------------------------------------------------------------------------------------------------------#
+#5
+def find_2nd_max(list):
+    lista = list
+    if len(lista) > 1:
+        lista.sort()
+        andra_plats = lista[-2]
+        return andra_plats
+    elif len(list) <=1 :
+        return None
+    # först kollar vi om listan innehåller mer än 0 element, om de gör de så tillverkas en variabel lista som är list.
+    # Efter detta sorterar vi listan från minst till störst
+    # sen tar vi en variabel andra_plats som då skall vara andra elementet från höger alltså -2 vilket är de nästa största talet i en sorterad lista.
+    #Finns inget tall returnas None
 
 
-def test sum_list():
+
